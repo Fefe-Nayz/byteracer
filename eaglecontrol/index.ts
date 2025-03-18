634add840c48c522d22c86b32730fb5595c99b93
NayZ.Sodium <55506519+nayzflux@users.noreply.github.com>
2025-03-18 15:27:33 +0100

    socket changes

--- eaglecontrol/index.ts
+++ eaglecontrol/index.ts
@@ -7,9 +7,11 @@ const app = new Hono();
 const { upgradeWebSocket, websocket } = createBunWebSocket<ServerWebSocket>();
 
 const cars = new Map<string, Car>();
+const clients = new Map<string, any>();
 
 type Car = {
   id: string;
+  socket: any;
 };
 
 type WebSocketEventName = "ping" | "pong" | "car_ready" | "gamepad_input";
@@ -20,12 +22,24 @@ type WebSocketEvent = {
   createdAt: number;
 };
 
+function broadcast(event: any) {
+  for (const socket of clients.values()) {
+    socket.send(event);
+  }
+}
+
 app.get(
   "/ws",
   upgradeWebSocket((c) => {
     return {
+      onOpen(ws) {
+        const uuid = "1";
+        clients.set(uuid, ws);
+        console.log("Client connected");
+      },
       onMessage(message, ws) {
         const event = JSON.parse(message.data.toString()) as WebSocketEvent;
+
         switch (event.name) {
           case "car_ready":
             console.log("Car ready");
@@ -39,8 +53,6 @@ app.get(
 
           case "gamepad_input":
             console.log("Receiving gamepad_input");
-
-            // ws.send("", {})
 
             console.log({
               event,
